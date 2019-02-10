940491ba604bb8a52467b0e9bbe2083bd70b6b4f
chau1997 <nguyenminhchau11031997@gmail.com>
2019-02-10 11:17:28 +0700

    Create chongcoppy_chongchuotphai.js

---
+++ chongcoppy_chongchuotphai.js
@@ -0,0 +1,18 @@
+<!--chong copy bai viet ( add tren the <body>)-->
+<style type='text/css'>
+body{
+-webkit-touch-callout: none;
+-webkit-user-select: none;
+-moz-user-select: none;
+-ms-user-select: none;
+-o-user-select: none;
+user-select: none;
+}
+</style>
+<!-- chong chuot phai ( add tren the <body>)-->
+<script type='text/javascript'>
+//<![CDATA[
+// JavaScript Document
+var message="NoRightClicking"; function defeatIE() {if (document.all) {(message);return false;}} function defeatNS(e) {if (document.layers||(document.getElementById&&!document.all)) { if (e.which==2||e.which==3) {(message);return false;}}} if (document.layers) {document.captureEvents(Event.MOUSEDOWN);document.onmousedown=defeatNS;} else{document.onmouseup=defeatNS;document.oncontextmenu=defeatIE;} document.oncontextmenu=new Function("return false")
+//]]>
+</script>
