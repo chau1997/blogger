0546908c8b5daa3b8b8ab20741c48765a522b79e
chau1997 <nguyenminhchau11031997@gmail.com>
2019-02-13 07:31:07 +0700

    Create API123link.js

---
+++ API123link.js
@@ -0,0 +1,9 @@
+<!-- kiem tien rut gon link -->
+  <script type='text/javascript'>
+            var adlinkfly_url = &#39;https://123link.co/&#39;;
+            var adlinkfly_api_token = &#39;1e37261536e9e5ce8da1d143568286996da3781b&#39;;
+            var adlinkfly_advert = 2;
+            var adlinkfly_domains = [&#39;drive.google.com&#39;, &#39;mega.nz&#39;, &#39;mshare.io&#39;,&#39;fshare.vn&#39;,&#39;mediafire.com&#39;,];
+        </script>
+        <script src='//123link.pro/js/script.js'/>
+<!--end -->
